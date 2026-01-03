2882dcee884dcc27f7efb27cf1b10cea595b8ab1
Hrishi-524 <sp3567208@gmail.com>
2026-01-03 18:57:02 +0530

    Env chnages

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 // frontend/src/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("https://metaterminal.duckdns.org", {
+export const socket = io(import.meta.env.VITE_SOCKET_URL, {
     path: "/socket.io/",  // Add this
     withCredentials: true,
     transports: ["websocket", "polling"], // Try both, prefer websocket
