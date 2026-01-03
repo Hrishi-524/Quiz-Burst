aa709e0073763e44ba5d5bb8cff3587bd74fbdc7
sp3567208@gmail.com <Hrishi-524>
2026-01-03 22:39:39 +0530

    started tarcking package-lock.json

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import setupSocketHandlers from './src/socket/index.js';
 
 import 'dotenv/config';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3002;
 
 // Create HTTP server 
 const server = createServer(app);
