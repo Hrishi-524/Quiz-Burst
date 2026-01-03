e3e7af0cff33297dbc9a5da3f6d7205ada96561c
Hrishi-524 <sp3567208@gmail.com>
2026-01-03 19:05:06 +0530

    minor adjustment

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -3,8 +3,8 @@ import { jwtDecode } from 'jwt-decode'
 
 // Set up axios interceptor to automatically include token in requests
 export const setupAxiosInterceptors = () => {
-    axios.defaults.baseURL =  import.meta.env.BACKEND_REQUEST_URL;
-    console.log(axios.defaults.baseURL)
+    axios.defaults.baseURL =  import.meta.env.BACKEND_REQUEST_URL || 'https://metaterminal.duckdns.org/quizburst/api'; 
+    console.log('This is axios Interceptors base url: ',axios.defaults.baseURL)
 
     // Request interceptor to add token to headers
     axios.interceptors.request.use(
