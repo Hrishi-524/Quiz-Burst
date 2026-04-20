c49b5cb2e5e185300861f35d63b2916218d67050
sp3567208@gmail.com <Hrishi-524>
2026-04-20 23:22:21 +0530

    Added new origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,28 +13,30 @@ const app = express();
 
 // Middleware - FIX CORS
 app.use(cors({ 
-  origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'https://quiz-burst.vercel.app',
-    'https://quizburst.route53.xyz',
-    'https://route53.xyz'
-    ];
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['Content-Disposition']
+    origin: function(origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        
+        const allowedOrigins = [
+            'http://localhost:5173',
+            'http://localhost:5174',
+            'https://quiz-burst.vercel.app',
+            'https://quizburst.route53.xyz',
+            'https://route53.xyz',
+            'https://quizburst.api.hrishi-developer.in',
+            'https://hrishi-developer.in'
+        ];
+        
+        if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+        } else {
+        callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Disposition']
 }));
 
 app.use(json());
